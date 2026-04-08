b30c88d52e2ec22dd4dd8a6803d6f5cb3e223537
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-04-08 12:59:42 +0200

    bump dependencies & versions

--- src/gtfs/import-trip-ids.ts
+++ src/gtfs/import-trip-ids.ts
@@ -8,7 +8,7 @@ export async function importTripIds(gtfsDirectory: string) {
 	const tripsContents = (await readFile(tripsPath)).toString();
 	const { data } = Papaparse.parse<{ trip_id?: string }>(tripsContents, {
 		header: true,
-    skipEmptyLines: true,
+		skipEmptyLines: true,
 	});
 	return data.map((trip) => trip.trip_id);
 }
